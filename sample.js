f1e1cb86d557fd8100da652c20359490d7df2c62
jannyHou <juehou@ca.ibm.com>
2017-10-15 20:43:56 -0400

    Add js file

---
+++ sample.js
@@ -0,0 +1,48 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const requiredMetadataKey = Symbol("required");
+class Greeter {
+    constructor(message) {
+        this.greeting = message;
+    }
+    greet(name) {
+        // the original example doesn't mark `name` as optional 
+        // but without a ? it proves the property is already a required...
+        return "Hello " + name + ", " + this.greeting;
+    }
+}
+__decorate([
+    validate,
+    __param(0, required)
+], Greeter.prototype, "greet", null);
+function required(target, propertyKey, parameterIndex) {
+    let existingRequiredParameters = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyKey) || [];
+    existingRequiredParameters.push(parameterIndex);
+    Reflect.defineMetadata(requiredMetadataKey, existingRequiredParameters, target, propertyKey);
+}
+function validate(target, propertyName, descriptor) {
+    let method = descriptor.value;
+    descriptor.value = function () {
+        let requiredParameters = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyName);
+        if (requiredParameters) {
+            for (let parameterIndex of requiredParameters) {
+                if (parameterIndex >= arguments.length || arguments[parameterIndex] === undefined) {
+                    throw new Error("Missing required argument.");
+                }
+            }
+        }
+        return method.apply(this, arguments);
+    };
+}
+let inst = new Greeter('hello');
+inst.greet();
